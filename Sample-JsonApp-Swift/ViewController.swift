4db34b4bbff2e32c043f649e9f4d32b30e951184
Yunnie Yunick <yunnieyunick@gmail.com>
2017-01-18 21:42:39 +0900

    add gitignore

---
+++ Sample-JsonApp-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample-JsonApp-Swift
+//
+//  Created by Yunnie Yunick on 2017/01/18.
+//  Copyright © 2017年 YunnieYunick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
